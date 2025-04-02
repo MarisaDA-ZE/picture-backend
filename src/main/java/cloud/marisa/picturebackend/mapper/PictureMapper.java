b3f8222471147aeb75b58166b46d819c05502426
MarisaDA-ZE <3038488053@qq.com>
2025-04-03 01:32:21 +0800

    feat: 初始化项目和前6章内容

---
+++ src/main/java/cloud/marisa/picturebackend/mapper/PictureMapper.java
@@ -0,0 +1,18 @@
+package cloud.marisa.picturebackend.mapper;
+
+import cloud.marisa.picturebackend.entity.dao.Picture;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author Marisa
+* @description 针对表【picture(图片表)】的数据库操作Mapper
+* @createDate 2025-03-29 22:12:31
+* @Entity cloud.marisa.picturebackend.entity.dao.Picture
+*/
+public interface PictureMapper extends BaseMapper<Picture> {
+
+}
+
+
+
+
---
+++ src/main/java/cloud/marisa/picturebackend/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package cloud.marisa.picturebackend.mapper;
+
+import cloud.marisa.picturebackend.entity.dao.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author Marisa
+* @description 针对表【user(用户表)】的数据库操作Mapper
+* @createDate 2025-03-28 15:58:26
+* @Entity cloud.marisa.picturebackend.entity.dao.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
